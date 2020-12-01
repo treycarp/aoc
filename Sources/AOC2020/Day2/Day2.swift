6501858a6c97e05e880177968603f6bf6fededed
Trey Carpenter <trey.carpenter@skookum.com>
2020-12-01 07:37:53 -0500

    Prepping for day 2

---
+++ Sources/AOC2020/Day2/Day2.swift
@@ -0,0 +1,18 @@
+//
+//  Day2.swift
+//  AOC2020
+//
+//  Created by Trey Carpenter on 12/1/20.
+//
+
+import Foundation
+
+final class Day2: Day {
+    override func part1() -> String {
+        return ""
+    }
+    
+    override func part2() -> String {
+        return ""
+    }
+}
