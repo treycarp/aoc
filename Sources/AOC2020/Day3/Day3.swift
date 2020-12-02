944239e64e6bdcccc09f34b2f1a3e0a9b41b74a1
Trey Carpenter <trey.carpenter@skookum.com>
2020-12-02 09:21:49 -0500

    Day 3 prep

---
+++ Sources/AOC2020/Day3/Day3.swift
@@ -0,0 +1,18 @@
+//
+//  Day3.swift
+//  AOC
+//
+//  Created by Trey Carpenter on 12/2/20.
+//
+
+import Foundation
+
+public class Day3: Day {
+    public override func part1() -> String {
+        ""
+    }
+    
+    public override func part2() -> String {
+        ""
+    }
+}
--- Sources/trey/main.swift
+++ Sources/trey/main.swift
@@ -2,5 +2,5 @@
 import AOC2020
 import AOC
  
-let d = AOC2020.Day2()
+let d = AOC2020.Day3()
 d.run()
