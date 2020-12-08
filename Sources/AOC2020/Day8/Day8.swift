40db8710f3be7a2b9bd91baa2b0ebda43ac163ff
Trey Carpenter <trey.carpenter@skookum.com>
2020-12-08 15:02:37 -0500

    Cleaned up a little.

--- Sources/AOC2020/Day8/Day8.swift
+++ Sources/AOC2020/Day8/Day8.swift
@@ -11,7 +11,7 @@ public class Day8: Day {
     public override func part1() -> String {
         let lines = Input().strings(name: "Day8Input.txt", year: "2020")
         
-        var ops: [Operation] = lines.compactMap {
+        let ops: [Operation] = lines.compactMap {
             let strings = $0.split(separator: " ")
             return Operation(instruction: Instruction(rawValue: String(strings[0]))!, argument: Int(String(strings[1]))!)
         }
@@ -19,22 +19,23 @@ public class Day8: Day {
         var accumulator = 0
         var currentIndex = 0
         var completed = false
+        var seen = Set<Int>()
         while !completed {
-            guard !ops[currentIndex].visited else {
+            guard !seen.contains(currentIndex) else {
                 completed = true
                 continue
             }
             switch ops[currentIndex].instruction {
             case .nop:
-                ops[currentIndex].visited = true
+                seen.insert(currentIndex)
                 currentIndex += 1
                 continue
             case .acc:
                 accumulator += ops[currentIndex].argument
-                ops[currentIndex].visited = true
+                seen.insert(currentIndex)
                 currentIndex += 1
             case .jmp:
-                ops[currentIndex].visited = true
+                seen.insert(currentIndex)
                 currentIndex += ops[currentIndex].argument
             }
         }
@@ -49,62 +50,57 @@ public class Day8: Day {
             let strings = $0.split(separator: " ")
             return Operation(instruction: Instruction(rawValue: String(strings[0]))!, argument: Int(String(strings[1]))!)
         }
-        let originalOps = ops
         var answer = 0
-        var foundAnswer = false
-        var outerIndex = 0
-        originalOps.forEach {
-            guard !foundAnswer else { return }
-            ops = originalOps
-            guard $0.instruction == .jmp || $0.instruction == .nop else {
-                outerIndex += 1
-                return
+        for (index, op) in ops.enumerated() {
+            guard op.instruction == .jmp || op.instruction == .nop else {
+                continue
             }
-            ops[outerIndex].instruction = ops[outerIndex].instruction == .jmp ? .nop : .jmp
-            var accumulator = 0
-            var currentIndex = 0
-            var infiniteLoop = false
-            repeat {
-                guard !ops[currentIndex].visited else {
-                    infiniteLoop = true
-                    break
-                }
-                
-                if currentIndex == ops.count - 1 {
-                    foundAnswer = true
-                }
-            
-                switch ops[currentIndex].instruction {
-                case .nop:
-                    ops[currentIndex].visited = true
-                    currentIndex += 1
-                    continue
-                case .acc:
-                    accumulator += ops[currentIndex].argument
-                    ops[currentIndex].visited = true
-                    currentIndex += 1
-                case .jmp:
-                    ops[currentIndex].visited = true
-                    currentIndex += ops[currentIndex].argument
-                }
-                
-                if foundAnswer {
-                    answer = accumulator
-                }
-    
-            } while currentIndex <= ops.count - 1 && !infiniteLoop && !foundAnswer
-            
-            outerIndex += 1
+            let instruction = ops[index].instruction
+            ops[index].instruction = ops[index].instruction == .jmp ? .nop : .jmp
+            if let theAnswer = doOp(ops: ops) {
+                answer = theAnswer
+                break
+            }
+            ops[index].instruction = instruction
         }
         
         return "Accumulator: \(answer)"
+    }
+    
+    private func doOp(ops: [Operation]) -> Int? {
+        var accumulator = 0
+        var currentIndex = 0
+        var seen = Set<Int>()
+        while currentIndex <= ops.count - 1 {
+            guard !seen.contains(currentIndex) else {
+                break
+            }
+        
+            switch ops[currentIndex].instruction {
+            case .nop:
+                seen.insert(currentIndex)
+                currentIndex += 1
+            case .acc:
+                accumulator += ops[currentIndex].argument
+                seen.insert(currentIndex)
+                currentIndex += 1
+            case .jmp:
+                seen.insert(currentIndex)
+                currentIndex += ops[currentIndex].argument
+            }
+            
+            if currentIndex == ops.count {
+                return accumulator
+            }
+        }
+        
+        return nil
     }
 }
 
 struct Operation {
     var instruction: Instruction
     let argument: Int
-    var visited: Bool = false
 }
 
 enum Instruction: String {
