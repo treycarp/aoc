bf47083c18ecfa5af876d940d343d2ebcf27d76b
Trey Carpenter <trey.carpenter@skookum.com>
2022-12-01 16:45:37 -0500

    Day 1

---
+++ AOC2022/AOC2022.swift
@@ -0,0 +1,8 @@
+//
+//  AOC2022.swift
+//  Advent of Code
+//
+//  Created by Trey Carpenter on 12/1/22.
+//
+
+import Foundation
---
+++ AOC2022/Day1/Day1.swift
@@ -0,0 +1,8 @@
+//
+//  Day1.swift
+//  Advent of Code
+//
+//  Created by Trey Carpenter on 12/1/22.
+//
+
+import Foundation
